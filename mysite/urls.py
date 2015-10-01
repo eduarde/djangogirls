eb81648989db086b326829682dc8d83e4e5af975
Eduard Erja <eduard.erja@gmail.com>
2015-10-02 00:28:41 +0300

    Added new app
    Added Home app, now we have navigation bar. Home & Blog

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('blog.urls')),
+     url(r'', include('home.urls')),
+    url(r'^blog/', include('blog.urls')),
+   
 ]
